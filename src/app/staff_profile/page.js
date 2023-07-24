f33a8cbd154b416fb636b19cba70ebf1c68d3512
Harsha662002 <hvasireddy329@gmail.com>
2023-07-24 22:03:07 +0530

    Updated Project

--- src/app/staff_profile/page.js
+++ src/app/staff_profile/page.js
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
+import React, { useContext, useEffect, useState } from "react";
+import Navbar from "../components/navbar/navbar";
+import { AuthContext } from "@/app/contexts/authcontext";
+import Sidebar from "../components/sidebar/sidebar";
+import StaffProfile from "../components/staff_profile/staff_profile";
 
 const StaffProfilePage = () => {
-  return <div>Hi</div>;
+  const { isLoggedIn, loggedInUserEmail } = useContext(AuthContext);
+  return (
+    <div>
+      <Navbar isLoggedIn={isLoggedIn} />
+      <div className="flex gap-6 flex-row">
+        <div>
+          <Sidebar />
+        </div>
+        <div className="text-xl w-screen h-1/2">
+          <StaffProfile />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StaffProfilePage;
